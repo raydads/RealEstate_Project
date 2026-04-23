d2eb356362bf3bc3427fa76969b128d7651127bb
Alex Zajicek <alex@macbookair.ad.tafensw.edu.au>
2026-04-23 10:11:24 +1000

    Initial commit

---
+++ agents/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+# username : Superuser
+# email : alex.zajicek1@gmail.com
+# password : Password 
---
+++ propertytrack/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='agents/login')
+def home(request):
+    return render(request, "propertytrack/home.html")
+    # CHANGE FROM propertytrack TO PROPERTYTRACK 
