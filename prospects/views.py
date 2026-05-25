47338536b3b33278a57f103ba562f939b1319c58
Alex Zajicek <alex@macbookair.ad.tafensw.edu.au>
2026-05-25 10:12:15 +1000

    Minor errors fixed

--- prospects/views.py
+++ prospects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Prospect
+from propertytrack.models import Inspection
 from .forms import ProspectForm
 # Create your views here.
 
